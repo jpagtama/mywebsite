ac8160a3fb91c36df59a0019c16e8be4cfcb518c
Julian Pagtama <jpagtama@gmail.com>
2022-08-01 10:26:16 -0700

    Adds Section wrapper component

---
+++ client/src/components/Section.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../styles/Section.module.css'
+
+const Section = props => {
+  return (
+    <section className={ styles.section } >{ props.children }</section>
+  )
+}
+
+export default Section;
