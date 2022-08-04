63b5ae60da05ee31e257fd76a55e1ff0ce035091
Julian Pagtama <jpagtama@gmail.com>
2022-08-03 19:53:52 -0700

    Adds SideNav component with css module

--- client/src/components/SideNav.jsx
+++ client/src/components/SideNav.jsx
@@ -1,9 +1,11 @@
-import styles from '../styles/SideNav.module.css';
 import Menu from './Menu';
+import { useSelector } from 'react-redux';
+import styles from '../styles/SideNav.module.css';
 
 const SideNav = () => {
+    const {showMenu} = useSelector(state => state.nav);
     return (
-        <nav className={ styles.nav } ><Menu /></nav>
+        <nav className={ `${styles.nav} ${showMenu? styles.openMenu: styles.closeMenu}`} ><Menu /></nav>
     )
 }
 export default SideNav;
