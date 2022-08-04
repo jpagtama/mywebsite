a8f9f89a37e8c5ea51b61d3bcf80e9ef81c0c88b
Julian Pagtama <jpagtama@gmail.com>
2022-08-03 19:53:04 -0700

    Adds Projects component with css module

---
+++ client/src/components/Projects.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+import { useInView } from 'react-intersection-observer';
+import {navActions} from '../store/navSlice';
+import styles from '../styles/Projects.module.css'
+
+const Projects = () => {
+  const dispatch = useDispatch();
+  const { ref, inView } = useInView({
+    threshold: 0,
+  });
+  useEffect(() => {
+    if (inView) dispatch(navActions.activate('projects'))
+  }, [inView, dispatch]);
+
+  return (
+    <div className={ styles.headingContainer }>
+      <h1 ref={ref} className={inView? styles.slideIn: ''}>Projects</h1>
+    </div>
+  )
+}
+
+export default Projects
