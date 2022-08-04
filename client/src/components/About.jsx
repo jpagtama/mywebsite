a3ff7eb6584612607828f888d478cc5789c628d8
Julian Pagtama <jpagtama@gmail.com>
2022-08-03 19:51:57 -0700

    Adds About component with css module

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { navActions } from '../store/navSlice'
+import { useDispatch } from 'react-redux'
 import LinkedInSVG from './svgs/LinkedInSVG'
 import GitHubSVG from './svgs/GitHubSVG'
 import { useInView } from 'react-intersection-observer';
@@ -6,18 +8,22 @@ import styles from '../styles/About.module.css'
 
 
 const About = () => {
+  const dispatch = useDispatch();
   const { ref, inView } = useInView({
     threshold: 0,
-    triggerOnce: true,
   });
+
+  useEffect(() => {
+    if (inView) dispatch(navActions.activate('about'));
+  }, [inView, dispatch]);
 
   return (
     <React.Fragment>
-        <span className={styles.headingContainer}>
+        <div className={styles.headingContainer}>
             <h1 ref={ref} className={inView? styles.slideIn : '' }>Julian Pagtama</h1>
             <h3>Software Developer &bull; Los Angeles, CA &bull; jpagtama@gmail.com</h3>
-        </span>
-        <span className={styles.summaryContainer}>
+        </div>
+        <div className={styles.summaryContainer}>
             <p className={ styles.summary }>
                 I have more than 6 years of software development experience in high volume applications where I aim to ensure scalability, optimize performance, and utilize best security practices.
             </p>
@@ -25,7 +31,7 @@ const About = () => {
                 <a href="/#linkedin" ><LinkedInSVG /></a>
                 <a href="/#github" ><GitHubSVG /></a>
             </div>
-        </span>
+        </div>
     </React.Fragment>
   )
 }
