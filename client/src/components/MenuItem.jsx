ab89135e1a39f31dcd06927398be53323452ccac
Julian Pagtama <jpagtama@gmail.com>
2022-08-05 12:50:09 -0700

    Modifies MenuItem to use new active styles

--- client/src/components/MenuItem.jsx
+++ client/src/components/MenuItem.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import styles from '../styles/MenuItem.module.css'
 
 const MenuItem = props => {
+
   return (
-    <li className={styles.listItem} >{ props.title }</li>
+    <li className={`${styles.listItem} ${props.isActive && styles.active}`} >{ props.title }</li>
   )
 }
 
