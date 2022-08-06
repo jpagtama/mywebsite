2aac4c2f4c67151d8561d65cdb5fc6e5cf28f1fe
Julian Pagtama <jpagtama@gmail.com>
2022-08-06 14:30:10 -0700

    Modifies footer content layout

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import styles from '../styles/Footer.module.css'
 
 const Footer = () => {
-  const date = new Date().getFullYear();
 
   return (
     <div className={styles.footerContainer}>
         <p className={ styles.sideNote }>
             This page was built using React and a NodeJS backend with Express. 
         </p>
-        <p className={ styles.sideNote }>Created by Julian Pagtama &copy; {date}</p>
+        <p className={ styles.sideNote }>Contact me at jpagtama@gmail.com</p>
+        <p className={ styles.sideNote }>Created by Julian Pagtama</p>
     </div>
   )
 }
