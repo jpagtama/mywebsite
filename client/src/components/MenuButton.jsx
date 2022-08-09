69b093e5d10133050727814e8ea6f7c269d2decf
Julian Pagtama <jpagtama@gmail.com>
2022-08-09 10:58:51 -0700

    Removes unnecessary code

--- client/src/components/MenuButton.jsx
+++ client/src/components/MenuButton.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { navActions } from '../store/navSlice';
 import { useSelector, useDispatch } from 'react-redux';
-import MenuSVG from './svgs/MenuSVG';
 import styles from '../styles/MenuButton.module.css';
 
 const MenuButton = props => {
@@ -12,14 +11,11 @@ const MenuButton = props => {
   };
 
   return (
-    // <button className={styles.menuButton} onClick={clickHandler} >
-        // {/* <MenuSVG showMenu={showMenu} /> */}
-        <button className={styles.menuButton} onClick={clickHandler} >
-            <span className={`${styles.top} ${showMenu? styles.animateTopToX:''}`}></span>
-            <span className={`${styles.mid} ${showMenu? styles.animateMidToX:''}`}></span>
-            <span className={`${styles.bottom} ${showMenu? styles.animateBottomToX:''}`}></span>
-        </button>
-    // </button>
+    <button className={styles.menuButton} onClick={clickHandler} >
+        <span className={`${styles.top} ${showMenu? styles.animateTopToX:''}`}></span>
+        <span className={`${styles.mid} ${showMenu? styles.animateMidToX:''}`}></span>
+        <span className={`${styles.bottom} ${showMenu? styles.animateBottomToX:''}`}></span>
+    </button>
   )
 }
 
