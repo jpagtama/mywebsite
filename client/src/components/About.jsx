90f2dce67357326f8bbf1c26acef2ee829398511
Julian Pagtama <jpagtama@gmail.com>
2022-08-12 06:53:18 -0700

    Adds refs for animation and external links

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -9,27 +9,39 @@ import styles from '../styles/About.module.css'
 
 const About = () => {
   const dispatch = useDispatch();
-  const { ref, inView } = useInView({
+  const { ref: aboutRef, inView: aboutInView } = useInView({
     threshold: 0,
+  });
+  const { ref: summaryRef, inView: summaryInView } = useInView({
+    threshold: 0,
+    triggerOnce: true
+  });
+  const { ref: linkedInRef, inView: linkedInView } = useInView({
+    threshold: 0,
+    triggerOnce: true
+  });
+  const { ref: gitHubRef, inView: gitHubInView } = useInView({
+    threshold: 0,
+    triggerOnce: true
   });
 
   useEffect(() => {
-    if (inView) dispatch(navActions.activate('about'));
-  }, [inView, dispatch]);
+    if (aboutInView) dispatch(navActions.activate('about'));
+  }, [aboutInView, dispatch]);
 
   return (
     <React.Fragment>
         <div id="About" className={styles.headingContainer}>
-            <h1 ref={ref} className={inView? styles.slideIn : '' }>Julian Pagtama</h1>
+            <h1 ref={aboutRef} className={aboutInView? styles.slideIn : '' }>Julian Pagtama</h1>
             <h3>Software Developer &bull; Los Angeles, CA &bull; jpagtama@gmail.com</h3>
         </div>
         <div className={styles.summaryContainer}>
-            <p className={ styles.summary }>
+            <p ref={summaryRef} className={`${styles.summary} ${summaryInView? styles.animateSummary: ''}`}>
                 I have more than 6 years of software development experience in high volume applications where I aim to ensure scalability, optimize performance, and utilize best security practices.
             </p>
             <div className={ styles.links } >
-                <a href="/#linkedin" ><LinkedInSVG /></a>
-                <a href="/#github" ><GitHubSVG /></a>
+                <a ref={linkedInRef} className={`${linkedInView && styles.animateLinkedIn}`} href="https://www.linkedin.com/in/jpagtama/" target="_blank" rel="noreferrer"><LinkedInSVG /></a>
+                <a ref={gitHubRef} className={`${gitHubInView && styles.animateGitHub}`} href="https://github.com/jpagtama" target="_blank" rel="noreferrer"><GitHubSVG /></a>
             </div>
         </div>
     </React.Fragment>
