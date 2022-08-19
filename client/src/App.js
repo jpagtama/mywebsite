570bf4e03fa1291c0235a74749a56f167290d5dd
Julian Pagtama <jpagtama@gmail.com>
2022-08-19 15:51:17 -0700

    Supplies BrowserRouter & adds routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
+import {Routes, Route} from 'react-router-dom';
 import SideNav from './components/personal/SideNav';
 import Main from './components/personal/Main';
+import Flekin from './components/flekin/Flekin';
 
 const App = () => {
+  const welcome = <React.Fragment><SideNav /><Main /></React.Fragment >
+
   return (
     <Provider store={store}>
-      <SideNav />
-      <Main />
+      <Routes >
+        <Route path="/" element={welcome} />
+        <Route path="/flekin/*" element={<Flekin />} />
+      </Routes>
     </Provider>
   )
 }
