42cb7b52730ab1f1549a016c5e71739ba689bae4
Julian Pagtama <jpagtama@gmail.com>
2022-08-22 19:56:28 -0700

    Lazy loads Flekin project

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 import {Routes, Route} from 'react-router-dom';
 import SideNav from './components/personal/SideNav';
 import Main from './components/personal/Main';
-import Flekin from './components/flekin/Flekin';
+
+const Flekin = React.lazy(() => import('./components/flekin/Flekin'));
 
 const App = () => {
   const welcome = <React.Fragment><SideNav /><Main /></React.Fragment >
 
   return (
     <Provider store={store}>
-      <Routes >
-        <Route path="/" element={welcome} />
-        <Route path="/flekin/*" element={<Flekin />} />
-      </Routes>
+      <Suspense >
+        <Routes >
+          <Route path="/" element={welcome} />
+          <Route path="/flekin/*" element={<Flekin />} />
+          <Route path="/*" element={welcome} />
+        </Routes>
+      </Suspense>
     </Provider>
   )
 }
